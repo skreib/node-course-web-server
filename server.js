78c7c74b5339164f8e4dd1d1c18753c7974f004e
Roman Vitruk <roman.vitruk@pdffiller.com>
2017-05-03 12:53:48 +0300

    Add project page

--- server.js
+++ server.js
@@ -3,24 +3,23 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-let app = express();
+var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-  let now = new Date().toString();
-  let log = `${now}: ${req.method} ${req.url}`;
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server.log.');
-    }
-  });
-
+  fs.appendFile('server.log', log + '\n');
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -34,20 +33,27 @@ hbs.registerHelper('screamIt', (text) => {
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: "About Page",
-    welcomeMessage: 'Hi there!'
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: "About Page",
+    pageTitle: 'About Page'
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
-    errorMassage: 'Unable to handle request'
+    errorMessage: 'Unable to handle request'
   });
 });
 
